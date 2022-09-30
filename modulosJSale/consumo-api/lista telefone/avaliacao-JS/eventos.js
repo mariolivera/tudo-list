eac083c0bef40937d12f51a7c8130730938e4c53
camylla-araujo <adriellysoares403@gmail.com>
2022-09-30 13:23:31 -0300

    criado lista e eventosjs

--- modulosJSale/consumo-api/lista telefone/avaliacao-JS/eventos.js
+++ modulosJSale/consumo-api/lista telefone/avaliacao-JS/eventos.js
@@ -1,19 +1,37 @@
-function adicionar(){
-    event.preventDefault();
-let = dados ={
-    item: input_item.value,
-    quantidade: parseInt(input_quantidade.value),
+const API_url =('http://localhost:8001')
+
+function atualizarlista() {
+    
+        fetch(API_url +'/lista/')
+        .then(function(resposta){
+
+            return resposta.json();
+        })
+        .then (function(listas){
+            tabela_telefonica.innerHTML ='';
+            listas.forEach(function(cadaItem){
+                tabela_telefonica.innerHTML +=`
+               <tr>
+                    <td>${cadaItem.id}</td>
+                    <td>${cadaItem.nome}</td>
+                    <td>${cadaItem.telefone}</td>
+                    <td>${cadaItem.cidade}</td>
+                        <button onclick="buscarparaeditar(${cadaItem.id})" data-bs-toggle="offcanvas" data-bs-target="#offcanvasEditar" class="btn btn-warning btn-sm">
+                            Editar
+                        </button>
+
+                        <button onclick="excluir(${cadaItem.id})" class="btn btn-danger">
+                            excluir
+                        </button>
+
+                       
+                    </td>    
+                </tr>
+                `; 
+            });
+        })
 }
 
-    fetch ('http://localhost:8001/lista',{
-    method: "POST",
-    Headers :{
-        'content.value':'aplication/json',
-    }
-})
-    body: JSON.stringfy(dados),
-    .then(resposta =>resposta.json())
-    .then(resposta=>atualizarlista());
-    
-}
-    form_add.Reset();
+atualizarlista();
+
+
