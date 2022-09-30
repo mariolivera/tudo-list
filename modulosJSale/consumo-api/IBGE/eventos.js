2e95306f86f04f1be714918a534c9f47f84d5261
mariolivera <113113372+mariolivera@users.noreply.github.com>
2022-09-30 12:19:21 -0300

    atualizando

--- modulosJSale/consumo-api/IBGE/eventos.js
+++ modulosJSale/consumo-api/IBGE/eventos.js
@@ -1,30 +1,44 @@
 fetch('https://servicodados.ibge.gov.br/api/v1/localidades/regioes')
 .then((resposta)=>resposta.json())
 .then((regioes)=>{
-        regioes.forEach( (cadaRegiao) => {
+        regioes.map( (cadaRegiao) => {
         document.getElementById('regiao').innerHTML +=`
-        <Option>${cadaRegiao.nome}</Option>
+        <Option ="${cadaRegiao.id}">${cadaRegiao.nome}</Option>
         `;
     });
 });
 
-fetch('https://servicodados.ibge.gov.br/api/v1/localidades/estados')
-.then((resposta)=>resposta.json())
-.then((Estado)=>{
-        Estado.forEach( (cadaEstado) => {
-        document.getElementById('estado').innerHTML +=`
-        <Option>${cadaEstado.nome}</Option>
-        `;
-    });
+function listarestado() {
+    document.getElementById('estado').innerHTML = <option="cadaEstado".value="0">-- selecione --</option>
+    let estadoselecionado =estado.value;    
+fetch('https://servicodados.ibge.gov.br/api/v1/localidades/estados/${estadoselecionado}')
+document.getElementById('estado').innerHTML +=
+    estado.forEach( (cadaEstado) => {
+    .then((resposta)=>resposta.json())
+    .then((Estado)=>{
+            console.log('estado');
+            let Estadostring = document.getElementById('estado').innerHTML
+            Estado.map((cadaEstado) => {
+                Cidadestring +=
+                <Option value ="${cadaEstado.id}">${cadaEstado.nome}</Option>
+                ;
+            });
+            document.getElementById('estado').innerHTML = Estadostring;
 });
+}
 
-fetch('')
-.then((resposta)=>resposta.json())
-.then((Cidade)=>{
-        Cidade.forEach( (cadaCidade) => {
-        document.getElementById('cidade').innerHTML +=`
-        <Option>${cadaCidade.nome}</Option>
-        `;
-    });
+function listarcidade() {
+    document.getElementById('cidade').innerHTML = <option value="0">-- selecione --</option>
+    let cidadeselecionado =cidade.value;    
+fetch('https://servicodados.ibge.gov.br/api/v1/localidades/estados/${estadoselecionado}/distrito')
+    .then((resposta)=>resposta.json())
+    .then((Estado)=>{
+            console.log('cidade');
+            let cidadestring = document.getElementById('cidade').innerHTML
+            cidade.map(cadacidade) => {
+                Cidadestring +=
+                <Option value ="${cadacidade.id}">${cadacidade.nome}</Option>
+                ;
+            });
+            document.getElementById('cidade').innerHTML = cidadestring;
 });
-
